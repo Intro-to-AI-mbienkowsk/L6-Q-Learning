3eda2448c6a0b2fe4b62c4b6fee90a137bf22c2d
maksym <01178511@pw.edu.pl>
2024-01-09 17:39:43 +0100

    implement the rest of the algorithm, add RewardSystem

--- src/Agent.py
+++ src/Agent.py
@@ -11,19 +11,42 @@ Observation = TypeVar('Observation')
 Action = TypeVar('Action')
 
 
+@dataclass(frozen=True)
+class FrozenLakeStepReport:
+    position: int
+    reward: float
+    terminated: bool
+    truncated: bool
+    info: dict
+
+
+@dataclass(frozen=True)
+class FrozenLakeObservation:
+    position: int
+    info: dict
+
+
 class Agent(ABC):
     def __init__(self, env: Env,
                  learning_rate: float = LEARNING_RATE,
                  n_episodes: int = N_EPISODES,
-                 epsilon: float = EPSILON,
                  discount_factor: float = DISCOUNT_FACTOR):
-        self.env = env
+        self._env = env
         self._lr = learning_rate
         self._n_episodes = n_episodes
-        self._epsilon = epsilon
-        # todo: qvalues setter
+        self._epsilon = 1
+        # todo: parametrize decay
+        self._epsilon_decay_rate = 1 / (self.n_episodes / 2)
         self._q_values = defaultdict(lambda: np.zeros(env.action_space.n))
         self._discount_factor = discount_factor
+
+    @property
+    def env(self) -> Env:
+        return self._env
+
+    @env.setter
+    def env(self, value: Env):
+        self._env = value
 
     @property
     def learning_rate(self) -> float:
@@ -45,9 +68,17 @@ class Agent(ABC):
     def q_values(self) -> defaultdict:
         return self._q_values
 
+    @q_values.setter
+    def q_values(self, val) -> None:
+        self._q_values = val
+
     @property
     def discount_factor(self) -> float:
         return self._discount_factor
+
+    @property
+    def epsilon_decay_rate(self) -> float:
+        return self._epsilon_decay_rate
 
     def get_random_action(self):
         return self.env.action_space.sample()
@@ -66,24 +97,56 @@ class Agent(ABC):
         delta = reward + self.discount_factor * best_next_q - self.q_values[obs][action]
         self.q_values[obs][action] += self.learning_rate * delta
 
+    def decay_epsilon(self):
+        self.epsilon = max(self.epsilon - self.epsilon_decay_rate, MIN_EPSILON)
+
+    @abstractmethod
+    def epoch(self):
+        ...
+
+    @abstractmethod
+    def train(self):
+        ...
+
+    @abstractmethod
+    def calculate_reward(self, obs: Observation):
+        ...
+
 
 class FrozenLakeAgent(Agent):
-    def __init__(self, env: Env):
+    def __init__(self, env: Env, reward_system: RewardSystem = BASE_REWARD_SYSTEM):
         super().__init__(env)
+        self.reward_system = reward_system
 
+    def epoch(self):
+        obs = FrozenLakeObservation(*self.env.reset())
+        terminal_state = False
+        while not terminal_state:
+            action = self.get_action(obs.position)
+            report = FrozenLakeStepReport(*self.env.step(action))
+            self.update(obs.position, action, report.reward, report.position)
+            terminal_state = report.terminated or report.truncated
+            obs = report
+        return obs
 
-@dataclass
-class FrozenLakeStepReport:
-    observation: int
-    reward: float
-    terminated: bool
-    truncated: bool
-    info: dict
+    def calculate_reward(self, obs: FrozenLakeStepReport) -> float:
+        """ Calculates the reward for a given move that conforms to self.reward_system
+         based on the outcome of the move"""
+        if obs.truncated:
+            return self.reward_system.on_fail
+        if obs.terminated:
+            return self.reward_system.on_success if obs.reward > 0 else self.reward_system.on_fail
+        return self.reward_system.on_nothing
 
-
-@dataclass
-class FrozenLakeObservation:
-    position: int
+    def train(self):
+        passed = 0
+        for e in range(self.n_episodes):
+            #   todo: calculate_reward(obs)
+            obs = self.epoch()
+            self.decay_epsilon()
+            passed += obs.reward
+            if obs.reward != 0:
+                print(f"Reward: {obs.reward}")
 
 
 class Move(Enum):
--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,17 @@
-LEARNING_RATE = .01
-N_EPISODES = 10000
-EPSILON = .9
-DISCOUNT_FACTOR = .9
-# TODO: epsilon decay
+from dataclasses import dataclass
 
+LEARNING_RATE = .01
+N_EPISODES = 100000
+DISCOUNT_FACTOR = .9
+MIN_EPSILON = .03
+
+
+@dataclass(frozen=True)
+class RewardSystem:
+    on_fail: float
+    on_success: float
+    on_nothing: float = 0
+
+
+BASE_REWARD_SYSTEM = RewardSystem(0, 1)
+HOLE_PENALTY_REWARD_SYSTEM = RewardSystem(-1, 1)
